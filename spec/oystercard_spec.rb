0f3dd93cd507b0a29f52bf7f42a4c103e26ac237
Sarah Moffat <sarah.moffat5@gmail.com>
2022-01-31 16:20:19 +0000

    Step 5 - Enable top up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,12 @@ describe Oystercard do
   it 'should have an opening balance of 0' do
     expect(subject.balance).to eq Oystercard::DEFAULT_BALANCE
   end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect { subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+  end
 end
