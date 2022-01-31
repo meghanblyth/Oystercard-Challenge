482e6b6e758a5f75ab9140855583f9296ba74d1b
meghanblyth <megblyth@gmail.com>
2022-01-31 16:51:18 +0000

    Step 6 - Enforce maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,15 @@
 class Oystercard
   DEFAULT_BALANCE = 0
+  MAXIMUM_BALANCE = 90
 
   attr_reader :balance
 
-  def initialize(balance = DEFAULT_BALANCE)
-    @balance = balance
+  def initialize
+    @balance = 0
   end
 
   def top_up(amount)
-    @balance = @balance + amount
+    fail 'You have reached the maximum possible balance of 90' if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
   end
 end
